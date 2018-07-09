6fd9ebabce589cd07512f2b16ca1daf746d25695
Jakub Prüher <jacobnzw@gmail.com>
2018-07-09 23:12:59 +0200

    Kalman filter update rule, Jacobian of the radar measurement implemented.

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -12,31 +12,39 @@ KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
                         MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-  x_ = x_in;
-  P_ = P_in;
-  F_ = F_in;
-  H_ = H_in;
-  R_ = R_in;
-  Q_ = Q_in;
+    x_ = x_in;
+    P_ = P_in;
+    F_ = F_in;
+    H_ = H_in;
+    R_ = R_in;
+    Q_ = Q_in;
+    I_ = MatrixXd::Identity(x_in.size(), x_in.size());
 }
 
 void KalmanFilter::Predict() {
-  /**
-  TODO:
-    * predict the state
-  */
+    /**
+     * Predict state.
+     */
+    x_ = F_*x_;
+    P_ = F_*P_*F_.transpose() + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Kalman Filter equations
-  */
+    /**
+     * Kalman filter update.
+     */
+    MatrixXd Pz = H_*P_*H_.transpose() + R_;
+    MatrixXd Pzx = H_*P_;
+    MatrixXd K = Pz.ldlt().solve(Pzx).transpose();
+    VectorXd e = z - H_*x_;
+    x_ = x_ + K*e;
+    P_ = (I_ - K*H_)*P_;
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-  /**
-  TODO:
-    * update the state by using Extended Kalman Filter equations
-  */
+    /**
+    TODO:
+      * update the state by using Extended Kalman Filter equations
+    */
+
 }
--- src/tools.cpp
+++ src/tools.cpp
@@ -27,8 +27,17 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     /**
-     * TODO: Calculates Jacobian evaluated at point given by x_state
+     * Calculates Jacobian evaluated at point given by x_state.
      */
-    MatrixXd a;
-    return a;
+    MatrixXd jacobian;
+    double p_x = x_state[0];
+    double p_y = x_state[1];
+    double v_x = x_state[2];
+    double v_y = x_state[3];
+    double norm2 = pow(p_x, 2) + pow(p_y, 2);
+    jacobian << p_x/(sqrt(norm2)), p_y/(sqrt(norm2)), 0, 0,
+                -p_y/norm2, p_x/norm2, 0, 0,
+                p_y * (v_x*p_y - v_y*p_x)/pow(norm2, 1.5), p_x * (v_y*p_x - v_x*p_y)/pow(norm2, 1.5),
+                p_x/sqrt(norm2), p_y/sqrt(norm2);
+    return jacobian;
 }
